fa7c9cb20085e9f502f1713a31de7e5b9aa8d467
Shorockhov-A <andr2409@bk.ru>
2022-02-07 15:15:45 +0300

    edited task_1_1

--- practical_task_1/task_1_1.py
+++ practical_task_1/task_1_1.py
@@ -3,7 +3,7 @@
 #    онлайн-конвертера преобразовать строковые представление в формат Unicode и также
 #    проверить тип и содержимое переменных.
 
-def words_type(*words):
+def words_type_print(*words):
     for item in words:
         print(item, type(item))
     print('----------------------------')
@@ -13,10 +13,10 @@ first_word = 'разработка'
 second_word = 'сокет'
 third_word = 'декоратор'
 
-words_type(first_word, second_word, third_word)
+words_type_print(first_word, second_word, third_word)
 
 first_word_unicode = '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430'
 second_word_unicode = '\u0441\u043e\u043a\u0435\u0442'
 third_word_unicode = '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440'
 
-words_type(first_word_unicode, second_word_unicode, third_word_unicode)
+words_type_print(first_word_unicode, second_word_unicode, third_word_unicode)
