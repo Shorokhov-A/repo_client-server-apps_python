dd952292a9653d48901cee6a9a8ec75018d3531e
Shorockhov-A <andr2409@bk.ru>
2022-03-25 13:19:16 +0300

    modified server module

--- practical_task_8/common/variables.py
+++ practical_task_8/common/variables.py
@@ -31,3 +31,12 @@ RESPONSE_DEFAULT_IP_ADDRESS = 'response_default_ip_address'
 MESSAGE = 'message'
 MESSAGE_TEXT = 'mess_text'
 EXIT = 'exit'
+
+# Словари-ответы:
+# 200
+RESPONSE_200 = {RESPONSE: 200}
+# 400
+RESPONSE_400 = {
+    RESPONSE: 400,
+    ERROR: None
+}
